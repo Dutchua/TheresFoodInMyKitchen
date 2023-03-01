fee0c280fa9c7822cb6d4d575dc0721ad319d003
Josh <josh.jennings5@gmail.com>
2023-03-01 17:02:35 +0200

    Now login with Google uses email

--- MysteryFoodAPI/Controllers/AuthenticationController.cs
+++ MysteryFoodAPI/Controllers/AuthenticationController.cs
@@ -141,7 +141,7 @@ namespace MysteryFoodApi.Controllers
 
             var payload = await GoogleJsonWebSignature.ValidateAsync(credential, settings);
 
-            var user = UserList.Where(x => x.Name == payload.Name).FirstOrDefault();
+            var user = UserList.Where(x => x.Email == payload.Email).FirstOrDefault();
 
             if (user != null)
             {
