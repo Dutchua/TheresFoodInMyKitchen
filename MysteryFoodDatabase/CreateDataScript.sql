d9c582ecff558fa643bd1f915cf032c71dda2aba
bbdnet2864 <kgothatsomoshoeshoe@gmail.com>
2023-03-01 15:06:29 +0200

    split create table and insert data queries

---
+++ MysteryFoodDatabase/CreateDataScript.sql
@@ -0,0 +1,49 @@
+
+CREATE TABLE ingredients(
+	ingredientId SERIAL PRIMARY KEY,
+	item CHARACTER VARYING(120) NOT NULL,
+	allergy BIT(1) NOT NULL,
+	status BIT(1) NOT NULL
+);
+
+CREATE TABLE Recipe(
+	recipeId SERIAL PRIMARY KEY,
+	description CHARACTER VARYING(120) NOT NULL,
+	instructions CHARACTER VARYING(10000)NOT NULL,
+	cuisineId INTEGER NOT NULL,
+	imageUrl CHARACTER VARYING(120) NULL,
+	status BIT(1) NOT NULL
+);
+
+CREATE TABLE RecipeIngredients(
+	recipeId INTEGER NOT NULL,
+	ingredientId INTEGER NOT NULL,
+	CONSTRAINT fk_recipeId FOREIGN KEY (recipeId)
+	REFERENCES recipe(recipeId),
+	CONSTRAINT fk_ingredientId FOREIGN KEY (ingredientId)
+	REFERENCES ingredients(ingredientId)
+	
+	
+);
+
+CREATE TABLE Rating(
+	ratingId SERIAL PRIMARY KEY,
+	recipeId INTEGER NOT NULL,
+	rating BIT(1) NOT NULL,
+	CONSTRAINT fk_recipeId foreign key (recipeId)
+	references recipe(recipeId)
+	
+);
+
+
+CREATE TABLE Cuisine(
+	ratingId SERIAL PRIMARY KEY,
+	cuisineType CHARACTER VARYING(120) NOT NULL
+);
+
+CREATE TABLE userInfo(
+	userId SERIAL PRIMARY KEY,
+	userEmail CHARACTER VARYING(120) NOT NULL,
+	userPassword CHARACTER VARYING(120) NOT NULL
+);
+
