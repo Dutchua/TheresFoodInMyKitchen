b4447017e550d25f14bcec38e6544bfdc2663437
Keaton <keanaido19@gmail.com>
2023-03-01 16:06:41 +0200

    Created Angular Project

---
+++ MysteryFoodFrontend/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from "./material/material.module";
+import {HttpClientModule} from "@angular/common/http";
+import {ReactiveFormsModule} from "@angular/forms";
+import {RegisterComponent} from './register/register.component';
+import {AppRoutingModule} from "./app-routing/app-routing.module";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ MysteryFoodFrontend/src/app/auth.service.ts
@@ -0,0 +1,38 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {environment} from "../environments/environment";
+import {Login} from "./model/Login";
+import {Register} from "./model/Register";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private path = environment.apiUrl;
+
+  constructor(private httpClient: HttpClient) { }
+
+  loginWithGoogle(credentials: string): Observable<any> {
+    const header = new HttpHeaders().set('Content-type', 'application/json');
+    return this.httpClient.post(
+      this.path + "LoginWithGoogle", JSON.stringify(credentials), { headers: header}
+    );
+  }
+
+  login(loginModel: Login): Observable<any> {
+    const header = new HttpHeaders().set('Content-type', 'application/json');
+
+    return this.httpClient.post(
+      this.path + 'Login', JSON.stringify(loginModel), { headers: header, withCredentials: true }
+    );
+  }
+
+  register(registerModel: Register): Observable<any> {
+    const header = new HttpHeaders().set('Content-type', 'application/json');
+
+    return this.httpClient.post(
+      this.path + 'Register', JSON.stringify(registerModel), { headers: header, withCredentials: true }
+    );
+  }
+}
