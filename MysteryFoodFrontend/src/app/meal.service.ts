96cb438e77404f3870d21ce539f9da7c645119a2
Josh <josh.jennings5@gmail.com>
2023-03-02 10:50:15 +0200

    Removed unnecessary methods from meal service

--- MysteryFoodFrontend/src/app/meal.service.ts
+++ MysteryFoodFrontend/src/app/meal.service.ts
@@ -1,41 +1,23 @@
-import { Injectable } from '@angular/core';
-import { HttpClient} from "@angular/common/http";
-import {Observable, of} from "rxjs";
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 import {Meal} from "./model/Meals";
 
- @Injectable({
+@Injectable({
   providedIn: 'root'
 })
 
 export class MealService {
 
    endPoint : string = "https://localhost:7274/Recipe";
-   meal:Object = []
-  constructor(private  http: HttpClient)
-  {}
-
+  constructor(private  http: HttpClient) {}
 
     getMeals(): Observable<Meal[]> {
-    // let options = {};
     return this.http.get<Meal[]>(this.endPoint);
     }
 
    getMeal(id: number): Observable<Meal> {
      return this.http.get<Meal>(`${this.endPoint}/${id}`);
-   }
-
-   rateMeal(id: number){
-     let options = {};
-     return this.http.patch(`${this.endPoint}/${id}`,options).subscribe(
-       value => {
-         this.meal = value;
-         console.log(this.meal);
-
-       }
-     );
-
-
-
    }
 
 }
